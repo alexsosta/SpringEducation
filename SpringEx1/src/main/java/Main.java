79ba92ff6a56e673e154e78001bb44333597bb2b
alexsosta <44973269+alexsosta@users.noreply.github.com>
2019-03-21 13:18:42 +0300

    21.03.2019 13:18

---
+++ SpringEx1/src/main/java/Main.java
@@ -0,0 +1,19 @@
+import entities.*;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class Main {
+    public static void main(String[] args) {
+        // создаем пустой спринговый контекст, который будет искать свои бины по аннотациям в указанном пакете
+        ApplicationContext context =
+                new AnnotationConfigApplicationContext("entities");
+
+        Cat cat = context.getBean(Cat.class);
+        Dog dog = (Dog) context.getBean("dog");
+        Parrot parrot = context.getBean("parrot-kesha", Parrot.class);
+
+        System.out.println(cat.getName());
+        System.out.println(dog.getName());
+        System.out.println(parrot.getName());
+    }
+}
